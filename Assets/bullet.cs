7d4e03a1c594b3097fd1f6db4d8e3ab4161a0ccc
syamamoto-pastoraldog <shimpei@pd-sy.local>
2016-11-01 11:02:07 +0900

    first commit

---
+++ Assets/bullet.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bullet : MonoBehaviour {
+
+  public int speed = 10;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody2D>().velocity = -transform.right.normalized * speed;
+
+	}
+}
---
+++ Assets/charactor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class charactor : MonoBehaviour {
+
+  public float speed = 15;
+
+  public GameObject bullet;
+  IEnumerator Start ()
+  {
+      while (true) {
+          // 弾をプレイヤーと同じ位置/角度で作成
+          Instantiate (bullet, transform.position, transform.rotation);
+          // 0.05秒待つ
+          yield return new WaitForSeconds (1.0f);
+      }
+  }
+
+
+	// Update is called once per frame
+	void Update () {
+	     float x = Input.GetAxisRaw ("Horizontal");
+
+        // 上・下
+        float y = Input.GetAxisRaw ("Vertical");
+
+        // 移動する向きを求める
+        Vector2 direction = new Vector2 (x, y).normalized;
+
+        // 移動する向きとスピードを代入する
+        GetComponent<Rigidbody2D>().velocity = direction * speed;
+	}
+}
