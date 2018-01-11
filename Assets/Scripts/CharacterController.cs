dd8298143b879c3e6be26b7c8f3e4ef23a020409
KaoruKawaguchi <kaorukawaguchi1030@live.jp>
2018-01-12 00:32:03 +0900

    端末対応

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -6,6 +6,7 @@ public class CharacterController: MonoBehaviour {
 	public float speed = 15;
 	public float JUMP_POW = 1000f;
 	public GameObject bullet;
+	public GameObject deviceInput;
 
 	Rigidbody2D rigidbody2d;
 	int groundLayer;
@@ -15,9 +16,16 @@ public class CharacterController: MonoBehaviour {
 	SpriteRenderer sprite;
 	Object prefab_eff_dead;
 
+	float moveVect = 0;
 
 	// 死亡イベント
 	public event System.Action eventDead;
+
+	void Awake() {
+#if UNITY_ANDROID || UNITY_IOS
+		deviceInput.gameObject.SetActive(true);
+#endif
+	}
 
 	void Start() {
 		this.prefab_eff_dead = Resources.Load("Effect/eff_dead");
@@ -38,32 +46,56 @@ public class CharacterController: MonoBehaviour {
 		Debug.DrawRay(transform.position, Vector3.down, Color.green, -1);
 	}
 
-	void Controll() {
-		float x = Input.GetAxisRaw("Horizontal");
+	public void MoveRight(bool key_down) {
+		if(key_down) moveVect = 1;
+		else moveVect = 0;
+	}
 
-		if (sprite.flipX && 0 < x) {
+	public void MoveLeft(bool key_down) {
+		if(key_down) moveVect = -1;
+		else moveVect = 0;
+	}
+
+	public void MoveX(float val) {
+
+		if (sprite.flipX && 0 < val){
 			// flipX==true は右向き
 			this.sprite.flipX = false;
 		}
-		else if (!sprite.flipX && x < 0) {
+		else if (!sprite.flipX && val < 0) {
 			this.sprite.flipX = true;
 		}
 
-		if (Mathf.Abs(x) > 0) {
+		if (Mathf.Abs(val) > 0) {
 			// 移動する向きとスピードを代入する
-			this.rigidbody2d.AddForce(Vector2.right * x * speed);
+			this.rigidbody2d.AddForce(Vector2.right * val * speed);
 		}
+	}
 
-		if (Input.GetButtonDown("Jump") && this.is_grounded) {
-			// ジャンプ
+	public void Jump() {
+		if (this.is_grounded) {
 			this.rigidbody2d.AddForce(Vector2.up * JUMP_POW);
 			this.is_grounded = false;
 		}
+	}
 
+	void Controll() {
+		float x = Input.GetAxisRaw("Horizontal");
+#if UNITY_ANDROID || UNITY_IOS
+		x = moveVect;
+#endif
+		MoveX(x);
+
+		if (Input.GetButtonDown("Jump")) {
+			// ジャンプ
+			Jump();
+		}
+#if UNITY_EDITOR
 		if (Input.GetButtonDown("Fire1")) {
 			// 弾発射
 			Fire();
 		}
+#endif
 	}
 
 	void Fire() {
